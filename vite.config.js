e1dd499fee62baf60c5389935346484c2de1baa4
SAMANTA NAYAK <samanta.nayak@gmail.com>
2025-12-27 14:36:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/action-item-creator/', // Replace with your repo name
+})
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
